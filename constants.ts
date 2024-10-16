b468fdb515040b20a29767507ec29d415066b030
AlexandrLegchilov <Alexandr_Legchilov@epam.com>
2024-10-16 09:28:02 +0300

    add folder structure

--- constants.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  PASSWORD: 'Nobody is gonna know',
-  VALUE: 'They are gonna know',
-  ENCRYPT_TEST_ENV_PATH: 'encrypt_test.env',
-  ENCRYPT_TEST_ENV_ENC_PATH: 'encrypt_test.env.enc',
-  DECRYPT_TEST_ENV_ENC_PATH: 'decrypt_test.env.enc',
-  DECRYPT_TEST_DECRYPTED_ENV_PATH: 'decrypted_decrypt_test.env',
-}
